ade7f08f2243d19a41d78dfded1559424a45748f
MCM Desarrollo <alberto.s@masconmenos.com.mx>
2023-06-16 15:50:42 -0600

    Principal View

--- backend/App/views/principal_all.php
+++ backend/App/views/principal_all.php
@@ -1,5 +1,5 @@
 <?php echo $header; ?>
-<div class="right_col">
+<div class="right_col" style="background: #ffffff;">
 
   <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
       <div style="text-align: center; padding-top: 90px;">
