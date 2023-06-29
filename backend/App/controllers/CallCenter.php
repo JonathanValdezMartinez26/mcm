2acaad6b676f691326c330d0d93511159073f6c0
MCM Desarrollo <alberto.s@masconmenos.com.mx>
2023-06-28 20:02:13 -0600

    Layout

--- backend/App/controllers/CallCenter.php
+++ backend/App/controllers/CallCenter.php
@@ -52,6 +52,7 @@ html;
 
         if ($credito != '' && $ciclo != '') {
             $Administracion = CallCenterDao::getAllDescription($credito, $ciclo);
+            var_dump($Administracion);
             foreach ($Administracion as $key => $value) {
                 $tabla .= <<<html
                 <div class="Contenedor1_resultados__XMhF5">
@@ -131,15 +132,28 @@ html;
                 </div></div>
 html;
             }
-            View::set('tabla', $tabla);
-            View::set('Administracion', $AdministracionOne);
-            View::set('header', $this->_contenedor->header($extraHeader));
-            View::set('footer', $this->_contenedor->footer($extraFooter));
-            View::render("callcenter_cliente_all");
-
+            if($Administracion[0] == '')
+            {
+                View::set('header', $this->_contenedor->header($extraHeader));
+                View::set('footer', $this->_contenedor->footer($extraFooter));
+                View::set('credito', $credito);
+                View::set('ciclo', $ciclo);
+                View::render("callcenter_cliente_message_all");
+            }
+            else
+            {
+                View::set('tabla', $tabla);
+                View::set('credito', $credito);
+                View::set('ciclo', $ciclo);
+                View::set('header', $this->_contenedor->header($extraHeader));
+                View::set('footer', $this->_contenedor->footer($extraFooter));
+                View::render("callcenter_cliente_all");
+            }
         } else {
             View::set('header', $this->_contenedor->header($extraHeader));
             View::set('footer', $this->_contenedor->footer($extraFooter));
+            View::set('credito', $credito);
+            View::set('ciclo', $ciclo);
             View::render("callcenter_all");
 
         }
@@ -147,153 +161,11 @@ html;
 
     public function Pendientes()
     {
-        $extraFooter = <<<html
-      <script>
-      
-         $(document).ready(function(){
-            $("#muestra-cupones").tablesorter();
-          var oTable = $('#muestra-cupones').DataTable({
-                "columnDefs": [{
-                    "orderable": false,
-                    "targets": 0
-                }],
-                 "order": false
-            });
-            // Remove accented character from search input as well
-            $('#muestra-cupones input[type=search]').keyup( function () {
-                var table = $('#example').DataTable();
-                table.search(
-                    jQuery.fn.DataTable.ext.type.search.html(this.value)
-                ).draw();
-            });
-            var checkAll = 0;
-            
-        });
-    
-      </script>
-html;
-
-        $fecha_inicio = $_GET['Inicial'];
-        $fecha_fin = $_GET['Final'];
-
-
-        if(empty($fecha_inicio) || empty($fecha_fin))
-        {
-            View::render("pagos_layout_all");
-        }
-        else
-        {
-            ///////////////////////////////////////////////////////////////////////////////////
-            $tabla = '';
-
-            $Layout = PagosDao::GeneraLayoutContable($fecha_inicio, $fecha_fin);
-
-            if ($Layout != '') {
-                foreach ($Layout as $key => $value) {
-
-                    $tabla .= <<<html
-                <tr style="padding: 0px !important;">
-                    <td style="padding: 0px !important;">{$value['FECHA']}</td>
-                    <td style="padding: 0px !important;">{$value['REFERENCIA']}</td>
-                    <td style="padding: 0px !important;">{$value['MONTO']}</td>
-                    <td style="padding: 0px !important;">$ {$value['MONEDA']}</td>
-                </tr>
-html;
-                }
-                if($Layout[0] == '')
-                {
-                    View::render("pagos_layout_busqueda_message");
-                }
-                else
-                {
-                    View::set('tabla', $tabla);
-                    View::set('footer', $this->_contenedor->footer($extraFooter));
-                    View::render("pagos_layout_busqueda");
-                }
-
-            } else {
-                View::render("pagos_layout_all");
-            }
-
-            ////////////////////////////////////////////////////
-        }
-
 
     }
 
     public function Historico()
     {
-        $extraFooter = <<<html
-      <script>
-      
-         $(document).ready(function(){
-            $("#muestra-cupones").tablesorter();
-          var oTable = $('#muestra-cupones').DataTable({
-                "columnDefs": [{
-                    "orderable": false,
-                    "targets": 0
-                }],
-                 "order": false
-            });
-            // Remove accented character from search input as well
-            $('#muestra-cupones input[type=search]').keyup( function () {
-                var table = $('#example').DataTable();
-                table.search(
-                    jQuery.fn.DataTable.ext.type.search.html(this.value)
-                ).draw();
-            });
-            var checkAll = 0;
-            
-        });
-    
-      </script>
-html;
-
-        $fecha_inicio = $_GET['Inicial'];
-        $fecha_fin = $_GET['Final'];
-
-
-        if(empty($fecha_inicio) || empty($fecha_fin))
-        {
-            View::render("pagos_layout_all");
-        }
-        else
-        {
-            ///////////////////////////////////////////////////////////////////////////////////
-            $tabla = '';
-
-            $Layout = PagosDao::GeneraLayoutContable($fecha_inicio, $fecha_fin);
-
-            if ($Layout != '') {
-                foreach ($Layout as $key => $value) {
-
-                    $tabla .= <<<html
-                <tr style="padding: 0px !important;">
-                    <td style="padding: 0px !important;">{$value['FECHA']}</td>
-                    <td style="padding: 0px !important;">{$value['REFERENCIA']}</td>
-                    <td style="padding: 0px !important;">{$value['MONTO']}</td>
-                    <td style="padding: 0px !important;">$ {$value['MONEDA']}</td>
-                </tr>
-html;
-                }
-                if($Layout[0] == '')
-                {
-                    View::render("pagos_layout_busqueda_message");
-                }
-                else
-                {
-                    View::set('tabla', $tabla);
-                    View::set('footer', $this->_contenedor->footer($extraFooter));
-                    View::render("pagos_layout_busqueda");
-                }
-
-            } else {
-                View::render("pagos_layout_all");
-            }
-
-            ////////////////////////////////////////////////////
-        }
-
 
     }
 
