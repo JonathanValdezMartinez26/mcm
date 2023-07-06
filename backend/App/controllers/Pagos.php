88e44ed212a818a4de950e73f4d642ed5e69758f
MCM Desarrollo <alberto.s@masconmenos.com.mx>
2023-07-06 12:01:01 -0600

    Horas

--- backend/App/controllers/Pagos.php
+++ backend/App/controllers/Pagos.php
@@ -760,7 +760,7 @@ html;
         if($this->__cdgco == '025' || $this->__cdgco == '026' || $this->__cdgco == '003'  || $this->__cdgco == '014'  || $this->__cdgco == '007'  || $this->__cdgco == '016')
         {
             // 025 - SUC TOLUCA 2// 026 - SUC TOLUCA 3 // 014 - SUC TOLUCA // 007 ZINA // 016
-            if($horaActual <= '11:10:00')
+            if($horaActual <= '12:10:00')
             {
                 if ($dia == 5)
                 {
