d0839cc054180f2ee671a0c8c2bd2cba6e5f88c9
MCM Desarrollo <alberto.s@masconmenos.com.mx>
2023-09-11 16:58:31 -0600

    PAgo

--- backend/App/views/horarios_caja_sucursal.php
+++ backend/App/views/horarios_caja_sucursal.php
@@ -3,7 +3,7 @@
     <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
         <div class="panel panel-body">
             <div class="x_title">
-                <h3> Asignación de Analistas a Sucursales</h3>
+                <h3> Asignación de Horarios a Sucursales</h3>
                 <div class="clearfix"></div>
             </div>
 
