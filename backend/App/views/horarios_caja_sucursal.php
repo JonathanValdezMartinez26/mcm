4b83f6c639c5894570b9f9c4f66f3c372e63e77e
MCM Desarrollo <alberto.s@masconmenos.com.mx>
2023-09-12 17:37:38 -0600

    PAgo

--- backend/App/views/horarios_caja_sucursal.php
+++ backend/App/views/horarios_caja_sucursal.php
@@ -140,7 +140,7 @@
                                             <option value="11:30:00">11:30 a.m</option>
                                             <option value="11:40:00">11:40 a.m</option>
                                             <option value="11:50:00">11:50 a.m</option>
-                                            <option value="06:50:00">06:50 p.m</option>
+                                            <option value="18:50:00">06:50 p.m</option>
                                         </select>
                                     </div>
                                 </div>
