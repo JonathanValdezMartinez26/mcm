b52be92d0c9cd2b6c18e69632c67a6e59d602081
MCM Desarrollo <alberto.s@masconmenos.com.mx>
2023-09-28 16:47:28 -0600

    Cambio

--- backend/App/models/CallCenter.php
+++ backend/App/models/CallCenter.php
@@ -204,7 +204,7 @@ sql;
              AND SPR.FECHA_TRABAJO BETWEEN TIMESTAMP '$fecha_inicio 00:00:00.000000' AND TIMESTAMP '$fecha_fin 23:59:59.000000'
              AND SEMAFORO = '1'
 sql;
-            var_dump($query);
+            //var_dump($query);
             return $mysqli->queryAll($query);
         }
         else
