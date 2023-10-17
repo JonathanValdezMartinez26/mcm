3ffc09dfb84dd05f770f107df57c72cea8e55cf9
MCM Desarrollo <alberto.s@masconmenos.com.mx>
2023-10-17 16:15:02 -0600

    Ejemplo7

--- backend/App/views/principal_call_center.php
+++ backend/App/views/principal_call_center.php
@@ -3,7 +3,76 @@
 
   <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
       <div style="text-align: center; padding-top: 10px;">
-           <img width="500" style="padding-top: 100px;" src="/img/logo.png">
+          <section class="content">
+              <div class="container-fluid">
+
+                  <div class="row">
+                      <div class="col-lg-3 col-6">
+
+                          <div class="small-box bg-info">
+                              <br>
+                              <div class="inner">
+                                  <h3>00<sup style="font-size: 20px"></sup></h3>
+                                  <p>Solicitudes en atraso</p>
+
+                                  <br>
+                              </div>
+                          </div>
+                      </div>
+
+                      <div class="col-lg-3 col-6">
+
+                          <div class="small-box bg-warning">
+                              <br>
+                              <div class="inner">
+                                  <h3>00</h3>
+                                  <p>Solicitudes Recibidas</p>
+                              </div>
+                              <div class="icon">
+                                  <i class="ion ion-person-add"></i>
+                              </div>
+                              <br>
+
+                          </div>
+                      </div>
+
+                      <div class="col-lg-3 col-6">
+
+                          <div class="small-box bg-success">
+                              <br>
+                              <div class="inner">
+                                  <h3>00<sup style="font-size: 20px"></sup></h3>
+                                  <p>Solicitudes Listas</p>
+                              </div>
+                              <div class="icon">
+                                  <i class="ion ion-stats-bars"></i>
+                              </div>
+                              <br>
+
+                          </div>
+                      </div>
+
+                      <div class="col-lg-3 col-6">
+
+                          <div class="small-box bg-danger">
+                              <br>
+                              <div class="inner">
+                                  <h3>00</h3>
+                                  <p>Solicitudes Pendientes</p>
+                              </div>
+                              <div class="icon">
+                                  <i class="ion ion-pie-graph"></i>
+                              </div>
+                              <br>
+
+                          </div>
+                      </div>
+
+                  </div>
+
+
+              </div>
+          </section>
       </div>
   </div>
 </div>
