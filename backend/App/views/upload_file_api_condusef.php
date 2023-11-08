bf33e42bfa1c3046a430713619059478a5664b98
MCM Desarrollo <alberto.s@masconmenos.com.mx>
2023-11-08 09:09:07 -0600

    Ejemplo

--- backend/App/views/upload_file_api_condusef.php
+++ backend/App/views/upload_file_api_condusef.php
@@ -16,10 +16,17 @@
                     <form class="" id="consulta" action="/Operaciones/ReportePLDPagos/" method="GET" onsubmit="return Validar()">
                         <div class="row">
                             <div class="col-md-12">
-
+                                <div class="col-md-4">
+                                    <select class="form-control mr-sm-3" style="font-size: 18px;" autofocus type="select" id="" name="" placeholder="000000" aria-label="Search">
+                                        <option value="1">Consulta</option>
+                                        <option value="2">Aclaración</option>
+                                        <option value="3">Reclamación</option>
+                                    </select>
+                                    <span id="availability1" style="font-size:15px"> Tipo de Documento *</span>
+                                </div>
                                 <div class="col-md-6">
                                     <input class="form-control mr-sm-2" autofocus type="file" id="myfile" name="myfile">
-                                    <span id="availability1" style="font-size:15px">.ZIP</span>
+                                    <span id="availability1" style="font-size:15px">.ZIP *</span>
                                 </div>
                                 <div class="col-md-2">
                                     <button class="btn btn-default" type="submit">Cargar</button>
