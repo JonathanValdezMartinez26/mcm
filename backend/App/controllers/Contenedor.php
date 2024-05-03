97eb84dee81b0bd080f61a87ecf0f8f75c745c5b
Jon <jvaldez_2610@hotmail.com>
2024-05-03 15:26:59 -0600

    Cambios

--- backend/App/controllers/Contenedor.php
+++ backend/App/controllers/Contenedor.php
@@ -106,7 +106,7 @@ html;
     if ($permiso_ahorro == '1' || $this->__usuario == 'LGFR' || $this->__usuario == 'PAES' || $this->__usuario == 'PMAB' || $this->__usuario == 'GASC') {
       $menu .= <<<html
            <hr>
-           <h3>General WEB AHORRO {} </h3>
+           <h3>General WEB AHORRO</h3>
             <ul class="nav side-menu">     
 html;
     }
