4a431d6e56ca96e23b3912291c07ee6801e8261d
Jon <jvaldez_2610@hotmail.com>
2024-05-23 18:44:12 -0600

    Cambio

--- backend/App/controllers/AdminSucursales.php
+++ backend/App/controllers/AdminSucursales.php
@@ -1372,8 +1372,10 @@ html;
 
             if ($value['CONCEPTO'] == 'TRANSFERENCIA INVERSION') {
                 $concepto = '<i class="fa fa-minus" style="color: #0000ac;"></i>';
-            } else if ($value['CONCEPTO'] == 'RETIRO' || $value['CONCEPTO'] == 'ENTREGA RETIRO PROGRAMADO' || $value['CONCEPTO'] == 'ENTREGA RETIRO EXPRESS') {
+            } else if ($value['CONCEPTO'] == 'RETIRO' || $value['CONCEPTO'] == 'ENTREGA RETIRO PROGRAMADO' || $value['CONCEPTO'] == 'ENTREGA RETIRO EXPRESS' || $value['CONCEPTO'] == 'RETIRO DE EFECTIVO') {
                 $concepto = '<i class="fa fa-arrow-up" style="color: #ac0000;"></i>';
+            }else if ($value['CONCEPTO'] == 'SALDO FINAL AL CIERRE DE LA SUCURSAL (DIARIO)') {
+                $concepto = '<i class="fa fa-dollar" style="color: #ff8600;"></i><i class="fa fa-dollar" style="color: #ff8600;"></i>';
             } else {
                 $concepto = '<i class="fa fa-arrow-down" style="color: #00ac00;"></i>';
             }
@@ -1550,7 +1552,7 @@ html;
                 $concepto = '<i class="fa fa-minus" style="color: #ac0000;"></i>';
             } else if ($value['CONCEPTO'] == 'TRANSFERENCIA INVERSIÓN (RECEPCIÓN)') {
                 $concepto = '<i class="fa fa-minus" style="color: #00ac00;"></i>';
-            } else if ($value['CONCEPTO'] == 'RETIRO' || $value['CONCEPTO'] == 'ENTREGA RETIRO PROGRAMADO' || $value['CONCEPTO'] == 'ENTREGA RETIRO EXPRESS') {
+            } else if ($value['CONCEPTO'] == 'RETIRO' || $value['CONCEPTO'] == 'ENTREGA RETIRO PROGRAMADO' || $value['CONCEPTO'] == 'ENTREGA RETIRO EXPRESS' || $value['CONCEPTO'] == 'RETIRO DE EFECTIVO') {
                 $concepto = '<i class="fa fa-arrow-up" style="color: #ac0000;"></i>';
             } else if ($value['TIPO_MOVIMIENTO'] == 'MOVIMIENTO VIRTUAL') {
                 $concepto = '<i class="fa fa-asterisk" style="color: #005dac;"></i>';
